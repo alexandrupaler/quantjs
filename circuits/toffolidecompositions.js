7b46244884a378b29377811d74f51f089b3c640d
Alexandru Paler <alexandrupaler@gmail.com>
2018-08-21 17:27:27 +0200

    added toffoli decomp. not impl.

---
+++ circuits/toffolidecompositions.js
@@ -0,0 +1,26 @@
+function ToffoliDecompositions()
+{
+    /*
+        A key-value store representing the decompositions
+        the gates are strings conforming to the unscheduledgate format
+    */
+    this.decompositions = {};
+}
+
+/*
+    There is a standard notation for the decomposition:
+    a is the first control
+    b is the second control
+    c is the target
+*/
+ToffoliDecompositions.prototype.addDecomposition = function(name, arrDecomposition)
+{
+    /*
+        strings
+    */
+}
+
+ToffoliDecompositions.prototype.applyDecomposition = function(gatePl, decompName)
+{
+    gatePl.
+}
